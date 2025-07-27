4b3057adc10fc673a4073826f3d40f72553f78db
Sergio Devs <sopekof@gmail.com>
2025-07-27 14:35:00 -0300

    Creacion de Login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,14 +5,12 @@ import { createAccessToken } from "../libs/jwt.js";
 import bcrypt from "bcryptjs";
 
 export const register = async (req, res) => {
-	const { email, password, username } = req.body;
-
-	//console.log(email, password, username);
-
 	// Aquí podrías agregar la lógica para guardar el usuario en la base de datos
 	// Instaciar un objeto de usario
 
 	try {
+		const { email, password, username } = req.body;
+
 		const passwordHash = await bcrypt.hash(password, 10); // --> esto da un string random
 
 		const newUser = new User({
@@ -27,12 +25,7 @@ export const register = async (req, res) => {
 		const token = await createAccessToken({ id: userSaved._id });
 
 		res.cookie("token", token);
-		res.json({
-			message: "Usario creadted sucessfully",
-		});
-
 		// Solo devolvemos datos para el frontend sin la contraseña
-
 		res.json({
 			id: userSaved._id,
 			username: userSaved.username,
@@ -45,6 +38,46 @@ export const register = async (req, res) => {
 	}
 };
 
-export const login = (req, res) => {
-	res.send("Login");
+export const login = async (req, res) => {
+	//console.log(email, password, username);
+
+	// Aquí podrías agregar la lógica para guardar el usuario en la base de datos
+	// Instaciar un objeto de usario
+
+	try {
+		const { email, password } = req.body;
+
+		const userFound = await User.findOne({ email });
+
+		if (!userFound) {
+			return res.status(400).json({ message: "User not found" });
+		}
+
+		const isMatch = await bcrypt.compare(password, userFound.password); // --> Devulve true o false
+
+		if (!isMatch) {
+			return res.status(400).json({ message: "Incorrect password" });
+		}
+
+		const token = await createAccessToken({ id: userFound._id });
+
+		res.cookie("token", token);
+		// Solo devolvemos datos para el frontend sin la contraseña
+		res.json({
+			id: userFound._id,
+			username: userFound.username,
+			email: userFound.email,
+			createdAt: userFound.createdAt,
+			updatedAt: userFound.updatedAt,
+		});
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+};
+
+export const logout = (req, res) => {
+	res.cookie("token", " ", {
+		expires: new Date(0), // -> Expira inmediatamente
+	});
+	return res.sendStatus(200);
 };
