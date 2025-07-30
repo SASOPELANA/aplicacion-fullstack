69de37a77af8de8a9eb7067d5e9a25c5593459b3
Sergio Devs <sopekof@gmail.com>
2025-07-30 18:20:32 -0300

    Crud - Task

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,10 +1,19 @@
 import { Router } from "express";
 import { authRequire } from "../middlewares/validateToken.js";
+import {
+	getTasks,
+	getTask,
+	createTask,
+	updateTask,
+	deleteTask,
+} from "../controllers/tasks.controller.js";
 
 const router = Router();
 
-router.get("/tasks", authRequire, (req, res) => {
-	res.json({ message: "Tasks" });
-});
+router.get("/tasks", authRequire, getTasks);
+router.get("/tasks/:id", authRequire, getTask);
+router.post("/tasks", authRequire, createTask);
+router.put("/tasks/:id", authRequire, updateTask);
+router.delete("/tasks/:id", authRequire, deleteTask);
 
 export default router;
